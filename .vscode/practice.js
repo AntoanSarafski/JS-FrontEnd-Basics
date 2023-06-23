00220c051463dce7eaa282b4bf1efad061f79f15
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-06-23 22:23:39 +0300

    1

--- .vscode/practice.js
+++ .vscode/practice.js
@@ -1,10 +1,9 @@
 function Sum(num1, num2) {
-  if (typeof num1 == "number" || typeof num2 !== "number") {
-    console.log("Wrong input types.");
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    console.log("Invalid input types.");
   } else {
     console.log(num1 + num2);
   }
 }
 
-let numForPrint = Sum("1", "2");
-console.log(numForPrint);
+Sum(1, 2);
