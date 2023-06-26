b60ac632bddcd20e58797819f73455d4e7309fff
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-06-26 23:04:01 +0300

    Updated JS Syntax Exercise
    Commit with 10.Same Digits

--- 01.JS-Syntax-Fundamentals-Lab/Exercise/10.Same-numbers/same-numbers.js
+++ 01.JS-Syntax-Fundamentals-Lab/Exercise/10.Same-numbers/same-numbers.js
@@ -1,14 +1,9 @@
 function checkIfDigitsAreSame(number) {
-  const digitString = number.toString();
-  let isConsistent = true;
-  let sum = Number(digitString[0]);
-
-  for (let index = 1; index < digitString.length; index++) {
-    sum += Number(digitString[index]);
-    if (digitString[index] !== digitString[index - 1]) {
-      isConsistent = false;
-    }
-  }
+  const digits = Array.from(String(number), Number);
+  const isConsistent = new Set(digits).size === 1;
+  const sum = digits.reduce(function (accumulator, current) {
+    return accumulator + current;
+  }, 0);
 
   console.log(isConsistent);
   console.log(sum);
