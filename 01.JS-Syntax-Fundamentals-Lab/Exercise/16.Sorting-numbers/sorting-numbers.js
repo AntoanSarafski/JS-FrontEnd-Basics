4cffe9a13a68b3af593dc1f92a23ec57ab43642c
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-06-28 19:02:00 +0300

    Updated JS Syntax Exercise
    Commit with 16.Sorting-Numbers.

--- 01.JS-Syntax-Fundamentals-Lab/Exercise/16.Sorting-numbers/sorting-numbers.js
+++ 01.JS-Syntax-Fundamentals-Lab/Exercise/16.Sorting-numbers/sorting-numbers.js
@@ -1,1 +1,19 @@
-function sortNumbers() {}
+function sortNumbers(arr) {
+  const sortedArray = arr.sort((a, b) => a - b);
+
+  const resultArray = [];
+
+  const length = arr.length;
+
+  for (let index = 0; index < length; index++) {
+    if (index % 2 === 0) {
+      resultArray.push(sortedArray.shift());
+    } else {
+      resultArray.push(sortedArray.pop());
+    }
+  }
+
+  console.log(resultArray);
+}
+
+sortNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);
