f55a2a017576a7587230022c4abac7d1b905af56
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-06-30 08:41:34 +0300

    Update words-uppercase.js
    Fixed bugs.

--- 01.JS-Syntax-Fundamentals-Lab/More-Exercise/02.Words-Uppercase/words-uppercase.js
+++ 01.JS-Syntax-Fundamentals-Lab/More-Exercise/02.Words-Uppercase/words-uppercase.js
@@ -1,7 +1,7 @@
 function wordsUppercase(text) {
   const result = text
     .split(/[\W]+/)
-    .filter((w) => w.length > 1)
+    .filter((w) => w.length >= 1)
     .map((w) => w.toUpperCase())
     .join(", ");
   console.log(result);
