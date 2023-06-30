a5165c7e6a626a5b89b1484466e8ee0ab857d63d
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-06-30 08:38:35 +0300

    Updated JS Syntax MORE EXERCISE
    Commit with 02. Words UpperCase.

---
+++ 01.JS-Syntax-Fundamentals-Lab/More-Exercise/02.Words-Uppercase/words-uppercase.js
@@ -0,0 +1,10 @@
+function wordsUppercase(text) {
+  const result = text
+    .split(/[\W]+/)
+    .filter((w) => w.length > 1)
+    .map((w) => w.toUpperCase())
+    .join(", ");
+  console.log(result);
+}
+
+wordsUppercase("Hi, how are you?");
